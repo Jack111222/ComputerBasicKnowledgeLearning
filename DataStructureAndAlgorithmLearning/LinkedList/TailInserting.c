fd5d0ebda3e225f6735853504f2d6b0e11b8a503
Jack111222 <13087212067@qq.com>
2020-11-26 21:51:43 +0800

    TailInserting.c

---
+++ DataStructureAndAlgorithmLearning/LinkedList/TailInserting.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct LNode
+{
+    int data;
+    struct LNode *next;
+}LNode;
+
+/*creating linked list by tail inserting method*/
+LNode *Create_LinkList()
+{
+    int data;
+    LNode *head = NULL, *tail = NULL, *p = NULL;
+
+    //create head node
+    head = tail = (LNode *)malloc(sizeof(LNode));
+    tail->next = NULL;
+
+    //create linked list
+    while(1)
+    {
+        scanf("%d", &data);
+        if(data == 0)
+        {
+            break;
+        }
+        else
+        {
+            p = (LNode *)malloc(sizeof(LNode));
+            p->data = data;  //data field assigment
+            p->next = NULL;
+            tail->next = p;
+            tail = p;  //the new created node is always the last node
+        }
+    }
+
+    return head;
+}
+
+int main()
+{
+    LNode *p;
+
+    p = Create_LinkList();
+    p = p->next;
+    while(p != NULL)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+
+    printf("\n");
+
+    return 0;
+}
